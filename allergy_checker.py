f85a94b446d429cd785594706dc522148d659a1d
Soohyeok Kim <sooo@unist.ac.kr>
2022-06-18 20:30:47 +0900

    closes #11

--- allergy_checker.py
+++ allergy_checker.py
@@ -1,11 +1,14 @@
 from PyQt5.QtWidgets import QDialog, QGridLayout, QVBoxLayout, QCheckBox, QDialogButtonBox
 from PyQt5 import QtCore
 from math import sqrt, ceil
+import json
 
 class AllergyWindow(QDialog):
     checklist = []
     def __init__(self, header):
         super().__init__()
+        with open('./data/settings.json', 'r') as f:
+            self.setting_data = json.load(f)
         self.initUI(header)
 
     def initUI(self, header):
@@ -27,6 +30,12 @@ class AllergyWindow(QDialog):
                 except IndexError:
                     break
         
+        for prev_checked in self.setting_data['prev_allergy']:
+            try:
+                self.cboxes[header.index(prev_checked)].toggle()
+            except ValueError:
+                continue
+
         vbox.addLayout(grid)
         self.buttonBox = QDialogButtonBox()
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
@@ -40,8 +49,12 @@ class AllergyWindow(QDialog):
     
     def getChecklist(self):
         checked = []
+        self.setting_data['prev_allergy'] = []
         for checkbox in self.cboxes:
             if checkbox.isChecked():
                 checked.append(checkbox.text())
+                self.setting_data['prev_allergy'].append(checkbox.text())
+        with open('./data/settings.json', 'w') as f:
+            json.dump(self.setting_data, f, indent = 2)
         self.checklist = checked
         self.close()
